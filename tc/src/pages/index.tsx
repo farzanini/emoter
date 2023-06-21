bc1534e237df6ffedf2223bab1d77899a956059d
Farzan <farzan.hosseiny@gmail.com>
2023-06-21 11:08:52 +0330

    Auth added

--- tc/src/pages/index.tsx
+++ tc/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
@@ -14,6 +15,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <SignInButton/>
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
