419aee27faa3a1e77a216a5617e3a070c1a7f5a6
Farzan <farzan.hosseiny@gmail.com>
2023-07-19 15:52:41 +0330

    posts working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,23 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage } from "~/components/Loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -31,7 +43,11 @@ const CreatePostWizard = () => {
         type="text"
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
@@ -102,7 +118,6 @@ const Home: NextPage = () => {
               </div>
             )}
             {isSignedIn && <CreatePostWizard />}
-            {!!isSignedIn && <SignOutButton />}
           </div>
           <Feed />
         </div>
